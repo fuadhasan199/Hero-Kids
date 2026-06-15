78c974f2e37d15967344856ef39512f16a30754c
fuadhasan199 <fuadhasan740266@gmail.com>
2026-06-15 23:24:35 +0600

    feat:products card implement in produts page

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
 
-const page = () => {
+
+
+ async function GetProducts(){
+
+ const res=await fetch(`http://localhost:3000/api/products`,{cache:"no-cache"}) 
+   return res.json()
+
+} 
+const page = async () => { 
+
+const products=await GetProducts() 
+  
+
     return (
-        <div className=' bg-gradient-to-r from-[#bcc0c7] to-[#c3cee0]  p-5'>
+        <div className=' bg-gradient-to-r from-[#bcc0c7] to-[#c3cee0]  p-5 '>
 
   {/* heading text */}
 <div className="text-center space-y-2 mb-4  py-7 rounded-[40px]">
@@ -44,13 +57,13 @@ const page = () => {
 </label> 
 
 
-{/* filter btn */} 
+{/* sorting btn */} 
 
 
 
 <div className="dropdown dropdown-hover">
-  <div tabIndex={0} role="button" className="btn m-2 bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600">Sort</div>
-  <ul tabIndex="-1" className="dropdown-content menu bg-base-content rounded-box z-1 w-52 p-2 shadow-sm">
+  <div tabIndex={0} role="button" className="btn m-2 bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600 mr-[80px]">Sort</div>
+  <ul tabIndex="-1" className="dropdown-content menu bg-base-content rounded-box z-1  w-52 p-5 shadow-sm">
     <li><a>default</a></li>
     <li><a>price low to high</a></li>
        <li><a>price high to low</a></li>
@@ -61,10 +74,80 @@ const page = () => {
 </div> 
 
 
-<p className='mt-12 font-bold text-2xl ml-5'>Total Products:</p>
+<p className='mt-12 font-bold text-2xl ml-5'>Total Products: {products.length}</p>
 
+ 
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+         {products?.map((pro) => {
+    const discount = pro.discount || pro.percentage || 0;
+    const oldPrice =
+      discount > 0
+        ? Math.round(pro.price / (1 - discount / 100))
+        : pro.price;
 
+    return (
+      <div
+        key={pro._id}
+        className="bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition duration-300 flex flex-col m-2 mt-8 "
+      >
+       
+        <div className="relative h-55 overflow-hidden">
+          <Image
+            src={pro.image}
+            alt={pro.title}
+            width={450}
+            height={450}
+            className="w-full h-full object-cover hover:scale-105 transition duration-300"
+          />
 
+          {discount > 0 && (
+            <span className="absolute top-3 left-3 bg-red-500 text-white text-xs px-3 py-1 rounded-full font-semibold">
+              -{discount}%
+            </span>
+          )}
+        </div>
+
+        {/* Content */}
+        <div className="p-4 flex flex-col flex-1">
+          <h2 className="font-bold text-lg line-clamp-2">
+            {pro.bangla}
+          </h2>
+
+          <p className="text-sm text-gray-500 mt-2 line-clamp-2">
+            {pro.description}
+          </p>
+
+          <div className="flex justify-between items-center mt-3 text-sm">
+            <span>⭐ {pro.ratings}</span>
+            <span>{pro.sold}+ Sold</span>
+          </div>
+
+          <div className="mt-3 flex items-center gap-2">
+            <span className="text-2xl font-bold text-orange-500">
+              ৳{pro.price}
+            </span>
+
+            {discount > 0 && (
+              <span className="text-gray-400 line-through text-sm">
+                ৳{oldPrice}
+              </span>
+            )}
+          </div>
+
+          <div className="mt-auto pt-5 flex gap-2">
+            <button className="btn btn-outline btn-primary flex-1">
+              View Details
+            </button>
+
+            <button className="btn btn-primary flex-1 text-white">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  })}
+</div> 
 
 
 
